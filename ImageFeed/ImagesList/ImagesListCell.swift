3cc5488fb7e31ae9a239bc029bdc1f8fe34f58f0
Adam West <118524522+Ad4mWest@users.noreply.github.com>
2023-05-30 19:30:11 +0200

    Compare the main screen with the image feed with the following parameters: the feed contains 20 photos; there is an indent between the photos; the edges of the photos are rounded; a label with the current date has been added to the photo; a like button has been added to the photo; the like is affixed to cells with an even index and is not affixed to an odd one; the dimensions, parameters and margins of the elements correspond to the layouts; the height of the cell depends on the size of the photo and is dynamic.

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//  ImagesListCell.swift
+//  ImageFeed
+//  Created by Adam West on 29.05.23.
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var imageViewCell: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+}
---
+++ SceneDelegate.swift
@@ -0,0 +1,22 @@
+//  SceneDelegate.swift
+//  ImageFeed
+//  Created by Adam West on 26.05.23.
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+     
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
